def39b4cfd6ffa73c8e19c991bb64019307b6143
Paintzoleg <paintzoleg@gmail.com>
2024-04-18 11:49:31 +0300

    Added index.html, script.js, style.css

---
+++ script.js
@@ -0,0 +1,9 @@
+let text = document.querySelector('.text');
+
+text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
+
+
+text.addEventListener('mouseover', function (event) {
+    if (event.target.tagName != 'SPAN') return;
+    event.target.classList.add('active');
+});
